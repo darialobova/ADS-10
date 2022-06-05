d04dbecb5bb6365042f92d97dfe5ad89f0d94c47
Лобова Дарья Сергеевна <94217366+darialobova@users.noreply.github.com>
2022-06-05 22:47:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,12 +6,10 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-
   std::string perm = tree[n - 1];
   std::vector<char> g;
   for (int x = 0; x < perm.length(); x++) {
     g.push_back(perm[x]);
   }
   return g;
-
 }
